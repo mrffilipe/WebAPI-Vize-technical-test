40f6e998b3efff805fc835b0ec9fad2f9613b2b3
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-07-25 14:03:17 -0300

    Add project structuring

---
+++ WebAPI-Vize-technical-test/src/Presentation/Interfaces/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Vize_technical_test.src.Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseController : ControllerBase
+    { }
+}
